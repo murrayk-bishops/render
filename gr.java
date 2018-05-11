cf6f78b1f2dcc81e52ef29fc820b22674e3fd87f
Kai <33099396+murrayk-bishops@users.noreply.github.com>
2018-05-11 12:04:12 -0700

    color gradient

--- gr.java
+++ gr.java
@@ -1,19 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.*;
 class MyPanel extends JPanel {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.green);
-		g.drawRect(0, 0, 200, 200);
-		for(int x = 0; x < 200; x++) {
-			Color c = new Color(x / 199f, 0f, 0f);
-			g.setColor(c);
-			g.drawLine(x, 0, x, 199);
+		BufferedImage image = new BufferedImage(400, 400, BufferedImage.TYPE_INT_RGB);
+		for(int y = 0; y < 400; y++) {
+			for(int x = 0; x < 400; x++) {
+				Color c = new Color(x / 399f, y / 399f, 1f - (float)Math.sqrt(Math.pow(x - 200, 2) + Math.pow(y - 200, 2)) / 400f);
+				image.setRGB(x, y, c.getRGB());
+			}
 		}
+		g.drawImage(image, 0, 0, null);
 	}
 	public Dimension getPrefferedSize() {
-		return new Dimension(200, 200);
+		return new Dimension(400, 400);
 	}
 }
 public class gr {
@@ -21,7 +23,7 @@ public class gr {
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("FRAME");
-		frame.setSize(201, 201);
+		frame.setSize(500, 500);
 		frame.setVisible(true);
 		frame.add(new MyPanel());
 	}
